017136545cf7a70b031d4de426416c2513c51c6c
Brian Byers <bbyers@pivotal.io>
2016-07-12 10:39:08 -0500

    fixing artifact path

--- ci/tasks/deploy-artifact.sh
+++ ci/tasks/deploy-artifact.sh
@@ -9,4 +9,4 @@ cf auth $CF_USER $CF_PASS
 echo "Create and Space"
 cf target -o $CF_ORG -s $CF_SPACE
 
-cf push -f git-repo/manifest.yml -n $CF_ROUTE -p {{ARTIFACT_PATH}}
+cf push -f git-repo/manifest.yml -n $CF_ROUTE -p $ARTIFACT_PATH
