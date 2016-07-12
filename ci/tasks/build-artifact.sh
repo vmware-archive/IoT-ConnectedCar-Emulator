d1e71b9051e04acefad4a8c8f1da57131386b240
Brian Byers <bbyers@pivotal.io>
2016-07-12 09:38:08 -0500

    starting to add pipeline

---
+++ ci/tasks/build-artifact.sh
@@ -0,0 +1,8 @@
+#!/usr/bin/env bash
+set -e
+
+export GRADLE_OPTS=-Dorg.gradle.native=false
+version=`cat version/number`
+cd git-repo
+gradle assemble -Pversion=$version
+cp build/libs/*.jar ../artifact-dir/
